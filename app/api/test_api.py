a6024705e2e26e9716c43a5b2cd0588f68cc1b64
clay <claysomes@outlook.com>
2023-03-07 22:32:46 +0800

    web

---
+++ app/api/test_api.py
@@ -0,0 +1,4 @@
+TEST = {
+    'api_key': "uzq9x9w7fv4UYfoufwdVJwPqtYYEkPJRRhLIA435RlIvd7Nv4LYolgg1vpbxAiTu",
+    'api_secret': "B0GlUrDaQXh1xJm7KldzIX0iMsvYjwf80lrk7ID3us3Z84r4kKasiuFETEVLxOcN"
+}
--- app/utils/web.py
+++ app/utils/web.py
@@ -8,22 +8,56 @@
 
 """Websocket connection based on binance connector"""
 
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
-import time
+from app.api.test_api import TEST
+api_key = TEST['api_key']
+api_secret = TEST['api_secret']
+
 import logging
+import time
 from binance.lib.utils import config_logging
-from binance.websocket.spot.websocket_stream import SpotWebsocketStreamClient as Client
+from binance.websocket.spot.websocket_api import SpotWebsocketAPIClient
+from binance.websocket.spot.websocket_stream import SpotWebsocketStreamClient
+from binance.spot import Spot as Client
+
 
 config_logging(logging, logging.DEBUG)
 
-def message_handler(_, message):
+def on_close(_):
+    logging.info("Do custom stuff when connection is closed")
+
+def api_message_handler(_, message):
+    logging.info("API: ")
     logging.info(message)
 
-my_client = Client(on_message=message_handler)
+def stream_message_handler(_, message):
+    logging.info("STREAM: ")
+    logging.info(message)
 
-my_client.ticker(symbol="bnbusdt")
 
-time.sleep(30)
+my_api_client = SpotWebsocketAPIClient(
+    api_key=api_key,
+    api_secret=api_secret,
+    on_message=api_message_handler,
+    on_close=on_close,
+)
 
-logging.debug("closing ws connection")
-my_client.stop()
+my_stream_client = SpotWebsocketStreamClient(on_message=stream_message_handler)
+
+spot_client = Client(api_key, api_secret)
+
+my_stream_client.ticker(symbol="btcbusd")
+
+my_api_client.account()
+time.sleep(2)
+
+logging.info(spot_client.account_snapshot("SPOT"))
+my_api_client.account()
+time.sleep(2)
+
+logging.info("closing ws connection")
+my_api_client.stop()
+my_stream_client.stop()
