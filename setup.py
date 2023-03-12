9bbc230dd6ea6e082ba64e4d49c4c64c1537910f
clay <claysomes@outlook.com>
2023-03-12 21:55:43 +0800

    setup_file_update

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name="seeker",
+    version="1.0.0",
+    packages=[
+        "seeker.app"
+    ],
+    description="quantitative trading framework.",
+)
